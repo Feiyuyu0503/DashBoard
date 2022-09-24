dc36e1d1084b4063be3d795a0c21d304558d8837
adlyq <adlyq_tim@qq.com>
2022-09-24 14:51:14 +0800

    [ci] 1. 修了点Bag 2. 更新一手版本号

--- app/src/main/java/com/dashboard/kotlin/MainActivity.kt
+++ app/src/main/java/com/dashboard/kotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.service.quicksettings.TileService
+import android.util.Log
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
@@ -11,19 +12,7 @@ import com.tencent.mmkv.MMKV
 import com.topjohnwu.superuser.BusyBoxInstaller
 import com.topjohnwu.superuser.Shell
 
-
-lateinit var GExternalCacheDir: String
-lateinit var KV: MMKV
-
 class MainActivity : AppCompatActivity() {
-    init {
-        Shell.setDefaultBuilder(
-            Shell.Builder.create()
-                .setInitializers(BusyBoxInstaller::class.java)
-                .setFlags(Shell.FLAG_REDIRECT_STDERR)
-        )
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,10 +27,6 @@ class MainActivity : AppCompatActivity() {
             android.R.color.transparent,
             applicationContext?.theme
         )
-
-        GExternalCacheDir = applicationContext.externalCacheDir.toString()
-        MMKV.initialize(this)
-        KV = MMKV.defaultMMKV(MMKV.MULTI_PROCESS_MODE, null)
 
         KV.putBoolean("TailLongClick", false)
         if (intent.action == TileService.ACTION_QS_TILE_PREFERENCES) {
